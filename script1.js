9174c6c0095ba7f4681adfc2ac87719ef6697e37
narendraravulapalli333 <95639403+narendraravulapalli333@users.noreply.github.com>
2022-02-14 13:45:46 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,4 +1,4 @@
-const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'
 const display = document.getElementById("text")
 const input = document.getElementById("input")
 const timer = document.getElementById("timer")
